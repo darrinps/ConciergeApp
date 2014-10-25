394742ea4990ed56d47a7eb24b66a9bf84f9d694
Caleb Penney <caleb.penney.75@gmail.com>
2014-10-25 18:29:58 -0500

    Add in new activity for second screen

---
+++ mobile/src/main/java/com/aa/consierge/gcm/PreferencesActivity.java
@@ -0,0 +1,30 @@
+package com.aa.consierge.gcm;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.aa.android.common.WearActivity;
+import com.aa.android.common.data.SharedDataEvent;
+import com.aa.consierge.R;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageEvent;
+
+public class PreferencesActivity extends WearActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.concierge_preferences);
+    }
+
+    @Override
+    public boolean handleMessageEvent(Context context, GoogleApiClient client, MessageEvent messageEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean handleDataEvent(Context context, GoogleApiClient client, SharedDataEvent dataEvent) {
+        return false;
+    }
+}
